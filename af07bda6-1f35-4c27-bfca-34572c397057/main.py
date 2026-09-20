b001d8014e562ffbdd96454c4d1b96b2f1701d10
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-09-20 15:36:02 +0200

    User Steven McLean saved changes to main.py at 09/20/2026, 13:36:01 UTC time.

--- af07bda6-1f35-4c27-bfca-34572c397057/main.py
+++ af07bda6-1f35-4c27-bfca-34572c397057/main.py
@@ -282,5 +282,19 @@ class TradingStrategy(Strategy):
                 }
 
             log("RISK-OFF")
+   # -----------------------------------------------------
+        # FINAL VALIDATION
+        # -----------------------------------------------------
 
+        allocation = self.normalize(allocation)
+
+        self.current_allocation = allocation
+
+        log(
+            "Target allocation: "
+            + str(allocation)
+        )
+
+        return TargetAllocation(allocation)
+        
             return TargetAllocation(allocation)
