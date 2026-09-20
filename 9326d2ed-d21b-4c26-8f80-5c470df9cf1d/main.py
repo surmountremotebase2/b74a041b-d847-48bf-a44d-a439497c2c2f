8e4ea94be6d5a3597302c661e4140154d4d243d6
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-09-20 22:02:53 +0200

    User Steven McLean saved changes to main.py at 09/20/2026, 20:02:52 UTC time.

--- 9326d2ed-d21b-4c26-8f80-5c470df9cf1d/main.py
+++ 9326d2ed-d21b-4c26-8f80-5c470df9cf1d/main.py
@@ -18,7 +18,7 @@ from math import log as ln, sqrt
 # PORTFOLIO SIZE
 # -------------------------------------------------------------
 
-NUMBER_OF_HOLDINGS = 15
+NUMBER_OF_HOLDINGS = 10
 
 
 # -------------------------------------------------------------
